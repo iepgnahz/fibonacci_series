a1c54433152877d842fec9885d92e25da90043fa
zhangpei <13488233237@163.com>
2016-06-01 22:08:30 +0800

    finsih fibonacci_series

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -1,7 +1,25 @@
 'use strict';
-
 function fibonacci_series(n) {
-
+  var arr = [0];
+  if(n == 1){
+    arr.push(1);
+  } else {
+    arr = [0,1];
+    for(var i = 2;i <= n; i++){
+      arr.push(fb(i));
+    }
+  }
+  return arr;
+}
+function fb(i){
+  if(i == 0) {
+    return 0;
+  }
+  if(i == 1) {
+    return 1;
+  } else {
+    return fb(i - 1) + fb(i - 2);
+  }
 }
 
 module.exports = fibonacci_series;
